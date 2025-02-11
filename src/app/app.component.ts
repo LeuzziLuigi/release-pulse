900f2ce7937222623a6bfab46fda3796f7302480
LuigiLeuzzi <leuzzi.luigi98@gmail.com>
2025-02-11 01:40:14 -0500

    setup

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,24 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'release-pulse';
+
+  menuOpen = false;
+  userSettingsOpen = false;
+
+  toggleMenu() {
+    this.menuOpen = !this.menuOpen;
+  }
+
+  toggleUserSettings(){
+    this.userSettingsOpen = !this.userSettingsOpen;
+  }
 }
